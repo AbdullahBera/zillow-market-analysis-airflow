f4f53f8c977fe7882cd80688e6591cac3f7145ce
AbdullahBera <kucukabdullahbera@gmail.com>
2025-02-04 21:41:27 -0800

    changes

--- scripts/save_cookies.py
+++ scripts/save_cookies.py
@@ -2,13 +2,13 @@ import pickle
 import undetected_chromedriver as uc
 import time
 
-# ✅ Force `undetected-chromedriver` to use Chrome version 131
+# Force `undetected-chromedriver` to use Chrome version 131
 driver = uc.Chrome(version_main=131)  
 
 driver.get("https://www.zillow.com/homes/San-Francisco,-CA_rb/")
 input("Manually solve CAPTCHA, then press Enter...")  # Wait for user to solve
 
-# ✅ Save cookies
+# Save cookies
 pickle.dump(driver.get_cookies(), open("../data/zillow_cookies.pkl", "wb"))
 driver.quit()
 print("✅ Cookies saved successfully!")
